799be2cff9db340f6f34dd5a9b890d67c9ea2d21
DanielTretyakov <denownload@vk.com>
2023-10-30 14:58:53 +0700

    New work

---
+++ Laba1.py
@@ -0,0 +1,114 @@
+class Worker:
+    def __init__(self, name, surname, age):
+        return
+
+    @property
+    def name(self):
+        return
+
+    @property
+    def surname(self):
+        return
+
+    @property
+    def age(self):
+        return
+
+    def __str__(self):
+        return
+
+
+class Boss:
+    def __init__(self, name, surname, age):
+        return
+
+    @property
+    def name(self):
+        return
+
+    @property
+    def surname(self):
+        return
+
+    @property
+    def age(self):
+        return
+
+    def __str__(self):
+        return
+
+
+class Work:
+    def __init__(self, name_of_work):
+        return
+
+    @property
+    def name_of_work(self):
+        return
+
+    @property
+    def boss(self):
+        return
+
+    @property
+    def workers(self):
+        return
+
+    def make_boss(self, boss):
+        return
+
+    def make_worker(self, worker):
+        return
+
+    def __str__(self):
+        return
+
+
+def create_work():
+    return
+
+
+def create_boss():
+    return
+
+
+def create_worker():
+    return
+
+
+def menu():
+    work = None
+    boss = None
+
+    while True:
+        print("Главное меню:")
+        print("1. Создать фирму.")
+        print("2. Создать директора(количество - 1).")
+        print("3. Создать сотрудника.(количество - не ограничено)")
+        print("4. Вывести информацию о сотруднике.")
+        print("5. Вывести информацию о директоре.")
+        print("6. Вывести информацию о фирме.")
+        print("7. Выход из программы ")
+
+        choose = input("Выберите пункт меню: ")
+
+        if choose == "1":
+            pass
+        elif choose == "2":
+            pass
+        elif choose == "3":
+            pass
+        elif choose == "4":
+            pass
+        elif choose == "5":
+            pass
+        elif choose == "6":
+            pass
+        elif choose == "7":
+            pass
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    menu()
